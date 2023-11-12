94930705689cfcdcc02f6c57c84fad060443a821
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-11-13 00:03:03 +0100

    Introduce draft for processing manager tests

---
+++ tests/test_processing_manager.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+
+from preprocessing import preprocessing_manager
+from preprocessing.processing_nodes import processing_node
+from preprocessing import audio_signal
+
+
+class MockPreprocessor(processing_node.ProcessingNode):
+    """Mock preprocessor that passes input signals forward."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def signature(self) -> str:
+        return "TestPreprocessor"
+
+    def _apply_transformations(self, signal: audio_signal.AudioSignal) -> audio_signal.AudioSignal:
+        return signal
+
+    def _transform_backwards(self, signal: audio_signal.AudioSignal) -> audio_signal.AudioSignal:
+        return signal
+
+    def _store_processing_info(self, signal: audio_signal.AudioSignal):
+        return
+
+
+def test_valid_processing():
+    """Runs processing and checks the outputs."""
+
+    input_path = os.path.join(
+        os.environ["TEST_RESOURCES"], "preprocessing_inputs")
+
+    output_path = os.path.join(
+        os.environ["TEST_RESULTS"], "preprocessing_outputs")
+
+    params = preprocessing_manager.ManagerParams(
+        input_path, output_path, [MockPreprocessor()], True, "*.wav")
+
+    # TODO: Implement test.
