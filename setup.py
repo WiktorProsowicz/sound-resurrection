1dfc21c38e2b846c00d57756a4776a0dbd531d09
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-11-13 23:15:50 +0100

    Update pre-commit config

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 """Script containing functions for managing the project workspace.
 
 Available functions and corresponding arguments are described in the
 main function as well as in the doc strings of the functions.
 """
-
 import argparse
+import os
 import pathlib
 import subprocess
 import venv
-import os
-from os import path, environ
+from os import environ
+from os import path
 
 HOME_PATH = pathlib.Path(__file__).absolute().parent.as_posix()
 
@@ -17,28 +18,30 @@ HOME_PATH = pathlib.Path(__file__).absolute().parent.as_posix()
 def run_tests() -> None:
     """Runs available tests from the 'tests' directory."""
 
-    tests_path = path.join(HOME_PATH, "tests")
-    src_path = path.join(HOME_PATH, "src")
+    tests_path = path.join(HOME_PATH, 'tests')
+    src_path = path.join(HOME_PATH, 'src')
 
-    test_results_path = path.join(HOME_PATH, "test_results")
+    test_results_path = path.join(HOME_PATH, 'test_results')
 
-    coverage_data_file = path.join(test_results_path, ".coverage")
-    coverage_stats_dir = path.join(test_results_path, "coverage_stats")
+    coverage_data_file = path.join(test_results_path, '.coverage')
+    coverage_stats_dir = path.join(test_results_path, 'coverage_stats')
 
     if not path.exists(test_results_path):
         os.makedirs(test_results_path)
 
     current_env = environ.copy()
-    current_env["PYTHONPATH"] = (src_path +
-                                 ":" + current_env.get("PYTHONPATH", ""))
-    current_env["TEST_RESOURCES"] = path.join(HOME_PATH, "tests", "res")
-    current_env["TEST_RESULTS"] = path.join(HOME_PATH, "test_results")
+    current_env['PYTHONPATH'] = (src_path +
+                                 ':' + current_env.get('PYTHONPATH', ''))
+    current_env['TEST_RESOURCES'] = path.join(HOME_PATH, 'tests', 'res')
+    current_env['TEST_RESULTS'] = path.join(HOME_PATH, 'test_results')
 
-    command = f"python3 -m coverage run --data-file={coverage_data_file} -m pytest --import-mode=prepend {tests_path}"
+    command = (f'python3 -m coverage run --data-file={coverage_data_file}'
+               f' -m pytest --import-mode=prepend {tests_path}')
 
     subprocess.run(command.split(), check=False, env=current_env)
 
-    command = f"python3 -m coverage html --data-file={coverage_data_file} --directory={coverage_stats_dir}"
+    command = (f'python3 -m coverage html --data-file={coverage_data_file}'
+               f'--directory={coverage_stats_dir}')
 
     subprocess.run(command.split(), check=False, env=current_env)
 
@@ -46,7 +49,7 @@ def run_tests() -> None:
 def setup_venv() -> None:
     """Sets up the virtual environment."""
 
-    venv_path = path.join(HOME_PATH, "venv")
+    venv_path = path.join(HOME_PATH, 'venv')
 
     venv.create(venv_path, with_pip=True, upgrade_deps=True)
 
@@ -72,14 +75,14 @@ def main(function: str, *args) -> None:
     raise RuntimeError(f"Couldn't find the function '{function}'.")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument(
-        "function_name", help="name of the function to be used")
+        'function_name', help='name of the function to be used')
     arg_parser.add_argument(
-        "args", nargs='*', help="positional arguments for the function")
+        'args', nargs='*', help='positional arguments for the function')
 
     arguments = arg_parser.parse_args()
 
