dce86cc1bdfc6f8f79b4320b953e97de8f8e54f2
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-11-24 10:20:49 +0100

    Modify setup.py
    Replaced `print` with logging lib
    Added test_results cleaning before tests run
    Added program description to args parser

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ Available functions and corresponding arguments are described in the
 main function as well as in the doc strings of the functions.
 """
 import argparse
+import logging
 import os
 import pathlib
+import shutil
 import subprocess
 import venv
 from os import environ
@@ -26,8 +28,16 @@ def run_tests() -> None:
     coverage_data_file = path.join(test_results_path, '.coverage')
     coverage_stats_dir = path.join(test_results_path, 'coverage_stats')
 
-    if not path.exists(test_results_path):
+    if not os.path.exists(test_results_path):
         os.makedirs(test_results_path)
+
+    # Cleaning test_results.
+    for root, dirs, files in os.walk(test_results_path):
+
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for directory in dirs:
+            shutil.rmtree(os.path.join(root, directory))
 
     current_env = environ.copy()
     current_env['PYTHONPATH'] = (src_path +
@@ -36,12 +46,12 @@ def run_tests() -> None:
     current_env['TEST_RESULTS'] = path.join(HOME_PATH, 'test_results')
 
     command = (f'python3 -m coverage run --data-file={coverage_data_file}'
-               f' -m pytest --import-mode=prepend {tests_path}')
+               f' -m pytest --import-mode=prepend -s {tests_path}')
 
     subprocess.run(command.split(), check=False, env=current_env)
 
     command = (f'python3 -m coverage html --data-file={coverage_data_file}'
-               f'--directory={coverage_stats_dir}')
+               f' --directory={coverage_stats_dir}')
 
     subprocess.run(command.split(), check=False, env=current_env)
 
@@ -51,13 +61,21 @@ def setup_venv() -> None:
 
     venv_path = path.join(HOME_PATH, 'venv')
 
-    venv.create(venv_path, with_pip=True, upgrade_deps=True)
+    if os.path.exists(venv_path):
+        logging.warning(
+            "Directory '%s' already exists. If you are sure you want to" +
+            ' replace it with a new environment, delete it and run again.', venv_path)
+        return
 
-    print(
-        f"Successfully created a virtual environment at directory '{venv_path}'")
-    print("You can now activate the environment with 'source ./venv/bin/activate'.")
-    print("Then type 'python3 -m pip install -r requirements.txt' to install dependencies.")
-    print("Then type 'deactivate' to deactivate the environment.")
+    venv.create(venv_path, with_pip=True, upgrade_deps=True, clear=False)
+
+    logging.info(
+        "Successfully created a virtual environment at directory '%s'", venv_path)
+    logging.info(
+        "You can now activate the environment with 'source ./venv/bin/activate'.")
+    logging.info(
+        "Then type 'python3 -m pip install -r requirements.txt' to install dependencies.")
+    logging.info("Then type 'deactivate' to deactivate the environment.")
 
 
 def main(function: str, *args) -> None:
@@ -77,7 +95,15 @@ def main(function: str, *args) -> None:
 
 if __name__ == '__main__':
 
-    arg_parser = argparse.ArgumentParser()
+    FUNCTION_DESCRIPTIONS = '\n'.join(
+        [f'{func.__name__}: {func.__doc__}' for func in [setup_venv, run_tests]])
+
+    PROGRAM_DESC = ('Script contains functions helping with project management.\n' +
+                    'Available functions:\n\n' +
+                    f'{FUNCTION_DESCRIPTIONS}')
+
+    arg_parser = argparse.ArgumentParser(
+        description=PROGRAM_DESC, formatter_class=argparse.RawDescriptionHelpFormatter)
 
     arg_parser.add_argument(
         'function_name', help='name of the function to be used')
