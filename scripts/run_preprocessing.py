95d99b927abfe3cbd3fdd3106f82150ff691c641
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-11-28 23:26:39 +0100

    Rework run_preprocessing script

--- scripts/run_preprocessing.py
+++ scripts/run_preprocessing.py
@@ -1,23 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Script runs preprocessing pipeline according to provided config."""
-
 import argparse
+from typing import Any
 from typing import Dict
-import yaml
-import os
-import librosa
 
-from preprocessing.processing_nodes import fragments_cutting_processor, processing_node
+import yaml  # type: ignore
+
 from preprocessing import preprocessing_manager
+from preprocessing.processing_nodes import downsampling_processor
+from preprocessing.processing_nodes import fragments_cutting_processor
+from preprocessing.processing_nodes import processing_node
 
-STR_TO_CLASS_DICT = {
-    'FragmentsCuttingProcessor': fragments_cutting_processor.FragmentsCuttingProcessor
+STR_TO_CLASS_DICT: Dict[str, type[processing_node.ProcessingNode]] = {
+    'FragmentsCuttingProcessor': fragments_cutting_processor.FragmentsCuttingProcessor,
+    'DownSamplingProcessor': downsampling_processor.DownSamplingProcessor,
 }
 
 
-def _spawn_processor(processor_spec: Dict[str, any]) -> processing_node.ProcessingNode:
+def _spawn_processor(processor_spec: Dict[str, Any]) -> processing_node.ProcessingNode:
+    """Creates a processing node according to provided specification.
 
-    processor_type = processor_spec["type"]
-    processor_cfg = processor_spec["processor_cfg"]
+    Args:
+        processor_spec: Dictionary containing fields describing the
+        processor and the specific processor's configuration.
+
+    Returns:
+        Spawned processor.
+
+    Raises:
+        ValueError: If the given processor type is unknown.
+    """
+
+    processor_type = processor_spec['type']
+    processor_cfg = processor_spec['processor_cfg']
 
     if processor_type in STR_TO_CLASS_DICT:
         return STR_TO_CLASS_DICT[processor_type].from_config(processor_cfg)
@@ -25,27 +41,28 @@ def _spawn_processor(processor_spec: Dict[str, any]) -> processing_node.Processi
     raise ValueError(f'Unknown processor type: {processor_type}')
 
 
-def _load_config(path: str) -> Dict[str, any]:
+def _load_config(path: str) -> Dict[str, Any]:
+    """Loads yaml config from the given path."""
 
-    with open(path, 'r') as config_file:
+    with open(path, 'r', encoding='utf-8') as config_file:
         return yaml.safe_load(config_file)
 
 
-def main(config: Dict[str, any]):
+def main(config: Dict[str, Any]):
     """Initializes preprocessing pipeline and runs it.
 
     Args:
         args: Script configuration.
     """
 
-    m_params = config["manager_params"]
+    m_params: Dict[str, Any] = config['manager_params']
 
-    sorted_processor_specs = sorted(m_params["processors"], key=lambda spec: spec["index"])
+    sorted_processor_specs = sorted(m_params['processors'], key=lambda spec: spec['index'])
     processors = [_spawn_processor(p_config) for p_config in sorted_processor_specs]
 
     manager_params = preprocessing_manager.ManagerParams(
-        m_params["input_path"], m_params["output_path"], processors,
-        m_params["search_recursively"], m_params["filter"]
+        m_params['input_path'], m_params['output_path'], processors,
+        m_params['search_recursively'], m_params['filter']
     )
 
     manager = preprocessing_manager.PreprocessingManager(manager_params)
@@ -53,12 +70,12 @@ def main(config: Dict[str, any]):
     manager.run_preprocessing()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description="Runs preprocessing pipeline according to provided config.")
+        description='Runs preprocessing pipeline according to provided config.')
 
-    parser.add_argument("config_path", help="path to the config file")
+    parser.add_argument('config_path', help='path to the config file')
 
     args = parser.parse_args()
 
