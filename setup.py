51307cf413a5e78548e4e4e5ddc0977941624142
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-11-29 23:34:03 +0100

    Add `run_script` function to setup.py

--- setup.py
+++ setup.py
@@ -40,8 +40,7 @@ def run_tests() -> None:
             shutil.rmtree(os.path.join(root, directory))
 
     current_env = environ.copy()
-    current_env['PYTHONPATH'] = (src_path +
-                                 ':' + current_env.get('PYTHONPATH', ''))
+    current_env['PYTHONPATH'] = f"{src_path}:{current_env.get('PYTHONPATH', '')}"
     current_env['TEST_RESOURCES'] = path.join(HOME_PATH, 'tests', 'res')
     current_env['TEST_RESULTS'] = path.join(HOME_PATH, 'test_results')
 
@@ -78,6 +77,31 @@ def setup_venv() -> None:
     logging.info("Then type 'deactivate' to deactivate the environment.")
 
 
+def run_script(script_name: str, *args) -> None:
+    """Runs a script from the 'scripts' directory.
+
+    Args:
+        script_name (str): Name of the script to be run.
+        args (list): List of values to be passed to the script via command line arguments.
+    """
+
+    script_path = path.join(HOME_PATH, 'scripts', script_name)
+
+    if not os.path.exists(script_path):
+        logging.error("Couldn't find the script '%s'.", script_path)
+        return
+
+    src_path = path.join(HOME_PATH, 'src')
+    current_env = os.environ.copy()
+    current_env['PYTHONPATH'] = f"{src_path}:{current_env.get('PYTHONPATH', '')}"
+
+    logging.info("Running script '%s'.", script_path)
+
+    command = f"python3 {script_path} {' '.join(args)}"
+
+    subprocess.run(command.split(), check=False, env=current_env)
+
+
 def main(function: str, *args) -> None:
     """Main function delegating the flow to other ones.
 
@@ -85,9 +109,9 @@ def main(function: str, *args) -> None:
         function (str): Name of the function to be called.
     """
 
-    for available_func in [setup_venv, run_tests]:
+    for available_func in [setup_venv, run_tests, run_script]:
         if available_func.__name__ == function:
-            available_func(*args)
+            available_func(*args)  # type: ignore
             return
 
     raise RuntimeError(f"Couldn't find the function '{function}'.")
@@ -95,8 +119,10 @@ def main(function: str, *args) -> None:
 
 if __name__ == '__main__':
 
-    FUNCTION_DESCRIPTIONS = '\n'.join(
-        [f'{func.__name__}: {func.__doc__}' for func in [setup_venv, run_tests]])
+    FUNCTION_DESCRIPTIONS_LIST = [f'{func.__name__}: {func.__doc__.splitlines()[0]}'  # type: ignore
+                                  for func in [setup_venv, run_tests, run_script]]
+
+    FUNCTION_DESCRIPTIONS = '\n'.join(FUNCTION_DESCRIPTIONS_LIST)
 
     PROGRAM_DESC = ('Script contains functions helping with project management.\n' +
                     'Available functions:\n\n' +
@@ -111,5 +137,7 @@ if __name__ == '__main__':
         'args', nargs='*', help='positional arguments for the function')
 
     arguments = arg_parser.parse_args()
+
+    logging.root.setLevel(logging.INFO)
 
     main(arguments.function_name, *arguments.args)
