d324ecaedba8666e8cdefb526fee95154b022ce1
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-12-09 22:35:36 +0100

    Finish preprocessing manager tests

--- tests/unit/preprocessing/test_preprocessing_manager.py
+++ tests/unit/preprocessing/test_preprocessing_manager.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
-import shutil
-from typing import Any, Dict
+import tempfile
+import wave
+from typing import Any
+from typing import Dict
 
 import pytest
 
@@ -11,14 +13,24 @@ from preprocessing.processing_nodes import processing_node
 
 
 class MockPreprocessor(processing_node.ProcessingNode):
-    """Mock preprocessor that passes input signals forward."""
+    """Passes forward the input signals and records the processing info.
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(True)
+    The sampling rate of the output signal is twice as high as the input one.
+    The number of processed signals is stored in the `number_of_processed_signals` property.
+    """
+
+    def __init__(self):
+        super().__init__(False)
+
+        self._number_of_processed_signals = 0
 
     @property
     def signature(self) -> str:
         return 'TestPreprocessor'
+
+    @property
+    def number_of_processed_signals(self) -> int:
+        return self._number_of_processed_signals
 
     def process(
             self, signal: audio_signal.AudioSignal) -> audio_signal.AudioSignal:
@@ -34,7 +46,11 @@ class MockPreprocessor(processing_node.ProcessingNode):
 
     def _apply_transformations(
             self, signal: audio_signal.AudioSignal, *args, **kwargs) -> audio_signal.AudioSignal:
-        return signal
+
+        new_signal_meta = signal.meta
+        new_signal_meta.sampling_rate *= 2
+
+        return audio_signal.AudioSignal(signal.data, new_signal_meta)
 
     def _transform_backwards(
             self, signal: audio_signal.AudioSignal) -> audio_signal.AudioSignal:
@@ -45,43 +61,97 @@ class MockPreprocessor(processing_node.ProcessingNode):
         return
 
 
-PREPROCESSING_OUTPUTS_PATH = os.path.join(
-    os.environ['TEST_RESULTS'], 'preprocessing_outputs')
+@pytest.fixture
+def processing_setup():
+    'Performs test directories cleaning and returns used paths.'
 
-
-def clean_path_recursively(path: str):
-    """Removes content of a path recursively."""
-
-    for root, dirs, files in os.walk(path):
-
-        for file in files:
-            os.remove(os.path.join(root, file))
-        for directory in dirs:
-            shutil.rmtree(os.path.join(root, directory))
-
-
-@pytest.fixture(autouse=True)
-def test_setup():
-    'Returns path to nested directories containing audio files.'
-
-    clean_path_recursively(PREPROCESSING_OUTPUTS_PATH)
-
-    yield
-
-
-def test_valid_processing_recursive():
-    """Runs processing and checks the outputs."""
-
-    input_path = os.path.join(
+    preprocessing_inputs_path = os.path.join(
         os.environ['TEST_RESOURCES'], 'preprocessing_inputs')
 
+    with tempfile.TemporaryDirectory() as preprocessing_outputs_path:
+        yield preprocessing_outputs_path, preprocessing_inputs_path
+
+# pylint: disable=redefined-outer-name
+
+
+def test_detects_invalid_input_paths(processing_setup):
+    """Checks if manager properly reacts to invalid input paths."""
+
+    outputs_path, _ = processing_setup
+
+    processor = MockPreprocessor()
+
     params = preprocessing_manager.ManagerParams(
-        input_path, PREPROCESSING_OUTPUTS_PATH, [MockPreprocessor()], True, '.*\\.wav')
+        'nonexisting_path', outputs_path, [processor], False, r'.*\.wav')
 
     manager = preprocessing_manager.PreprocessingManager(params)
 
     manager.run_preprocessing()
 
-    encountered_files = set(os.listdir(PREPROCESSING_OUTPUTS_PATH))
+    assert processor.number_of_processed_signals == 0
 
-    assert (encountered_files == {'file1.wav', 'file2.wav', 'file3.wav'})
+
+# pylint: disable=redefined-outer-name
+def test_creates_output_directory_if_does_not_exist(processing_setup):
+    """Checks if manager creates output directory if it does not exist."""
+
+    outputs_path, inputs_path = processing_setup
+    outputs_path = os.path.join(outputs_path, 'nonexisting_directory')
+
+    params = preprocessing_manager.ManagerParams(
+        inputs_path, outputs_path, [MockPreprocessor()], False, r'.*\.wav')
+
+    manager = preprocessing_manager.PreprocessingManager(params)
+
+    manager.run_preprocessing()
+
+    assert os.path.exists(outputs_path)
+
+
+@pytest.mark.parametrize('recursively,files_filter,expected', [
+    (True, r'.*\.wav', {'file1.wav', 'file2.wav', 'file3.wav'}),
+    (False, r'.*\.wav', {'file1.wav'}),
+    (False, r'.*1\.wav', {'file1.wav'}),
+    (True, r'.*1\.wav', {'file1.wav'})
+])
+@pytest.mark.dependency(depends=['signal_reader_test', 'signal_writer_test'], scope='session')
+# pylint: disable=redefined-outer-name
+def test_discovers_input_files_and_dumps_results(
+        processing_setup, recursively, files_filter, expected):
+    """Runs processing and checks the contents of the output directory."""
+
+    outputs_path, inputs_path = processing_setup
+
+    params = preprocessing_manager.ManagerParams(
+        inputs_path, outputs_path, [MockPreprocessor()], recursively, files_filter)
+
+    manager = preprocessing_manager.PreprocessingManager(params)
+
+    manager.run_preprocessing()
+
+    encountered_files = set(os.listdir(outputs_path))
+
+    assert encountered_files == expected
+
+
+@pytest.mark.parametrize('preprocessors,expected',
+                         [([MockPreprocessor()], 88200), ([MockPreprocessor(), MockPreprocessor()], 176400)])
+@pytest.mark.dependency(depends=['signal_reader_test', 'signal_writer_test'], scope='session')
+# pylint: disable=redefined-outer-name
+def test_properly_runs_processing(processing_setup, preprocessors, expected):
+    """Checks whether the processors actually process the input signals."""
+
+    outputs_path, inputs_path = processing_setup
+
+    params = preprocessing_manager.ManagerParams(
+        inputs_path, outputs_path, preprocessors, False, r'.*\.wav')
+    manager = preprocessing_manager.PreprocessingManager(params)
+
+    manager.run_preprocessing()
+
+    assert len(os.listdir(outputs_path)) == 1
+
+    with wave.open(os.path.join(outputs_path, 'file1.wav'), 'rb') as input_binary:
+        target_sr = input_binary.getframerate()
+
+        assert target_sr == expected
